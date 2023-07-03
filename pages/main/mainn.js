e0e981f7c3b1473b2e3b6060392f27b5d0cf0fb4
Iskandar <hasanoviskandar289@gmail.com>
2023-07-04 02:03:27 +0500

    market finish

--- pages/main/mainn.js
+++ pages/main/mainn.js
@@ -25,22 +25,23 @@ menuBtn.forEach(el => {
         el.classList.add('active-button')
 
         let page = document.querySelector(`.${data}_page`)
-        pages.forEach(pg => pg.classList.add('show'))
-        page.classList.remove('show')
+        pages.forEach(pg => pg.classList.remove('show'))
+        page.classList.add('show')
 
     }
 })
-    let marketPage = document.querySelector('.market_page')
+let marketPage = document.querySelector('.market_page .wrapper')
 
-axios.get('http://localhost:5050/market')
+axios.get('http://localhost:5050/market/')
 
     .then(res => {
         let arr = res.data
-        
-        for (let item of res.data) {         
-            console.log(arr.item);   
+        for (let i of arr) {
+           
             let marketCrypto = document.createElement('div')
+            let marketCryptoTop = document.createElement('div')
             let marketCryptoLeft = document.createElement('div')
+            let marketCryptoLeftTxt = document.createElement('div')
             let marketCryptoRight = document.createElement('div')
             let cryptoIcon = document.createElement('img')
             let cryptoName = document.createElement('p')
@@ -50,24 +51,47 @@ axios.get('http://localhost:5050/market')
             let btc = document.createElement('p')
             let course = document.createElement('h2')
             let volume = document.createElement('span')
-
+            let line =  document.createElement('img')
+            marketCrypto.classList.add('marketCrypto')                
+            marketCryptoLeft.classList.add('marketCryptoLeft')                                    
+            marketCryptoRight.classList.add('marketCryptoRight')                                
+            marketCryptoTop.classList.add('marketCryptoTop')       
             cryptoIcon.src = '/public/img/crypto/btc.svg'
-            cryptoName.innerHTML = res.data.name
-            cryptoPercent.innerHTML = res.data.percent
+            cryptoName.innerHTML = i.name
+            cryptoPercent.innerHTML = i.percent
             eur.innerHTML = 'EUR'
             usd.innerHTML = 'USD'
             btc.innerHTML = 'BTC'
-            course.innerHTML = res.data.course + 'EUR'
-            volume.innerHTML = volume + res.data.volume + res.data.name
+            course.innerHTML = i.course + ' ' + i.currency 
+            volume.innerHTML = "Volume " + i.volume + " " + i.name
             marketCryptoRight.append(eur, usd, btc, course, volume)
-            marketCryptoLeft.append(cryptoIcon, cryptoName, cryptoPercent)
-            marketCrypto.append(marketCryptoLeft, marketCryptoRight)
+            marketCryptoLeftTxt.append(cryptoName, cryptoPercent)
+            marketCryptoLeft.append(cryptoIcon, marketCryptoLeftTxt)
+            marketCryptoTop.append(marketCryptoLeft, marketCryptoRight)
+            marketCrypto.append(marketCryptoTop, line)
             marketPage.append(marketCrypto)
-            if (res.data.percent > 0) {
+            
+            if (i.percent > 0) {
                 cryptoPercent.style.color = '#00E8AC'
+                line.src = '/public/img/greenLine.png'
             } else {
                 cryptoPercent.style.color = '#F35050'
+                line.src = '/public/img/redLine.png'
             }
         }
+
     })
-    
+    import Chart from 'chart.js/auto'
+    const ctx = document.getElementById('myChart');
+
+    new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [1,],
+          borderWidth: 1
+        }]
+      }
+    });
